5d40bf5a587d30e333b2022de12b8a26baddeca2
anxdre <anxdre@protonmail.com>
2022-10-06 08:36:14 +0700

    Initial Commit

---
+++ app/src/main/java/com/anxdre/coffetycoon/ui/intro/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.anxdre.coffetycoon.ui.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anxdre.coffetycoon.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
